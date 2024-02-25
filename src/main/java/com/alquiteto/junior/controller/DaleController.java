14f8b3a3f6f8ed917575342d2d34af559e186e3d
Pedro Alcântara <pedroalcantara.info@gmail.com>
2024-02-25 14:42:30 -0300

    teile e zaga

--- src/main/java/com/alquiteto/junior/controller/DaleController.java
+++ src/main/java/com/alquiteto/junior/controller/DaleController.java
@@ -1,5 +1,9 @@
 package com.alquiteto.junior.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,13 +17,16 @@ import java.util.Random;
 @RestController
 @RequestMapping("/random")
 @Validated
+@RequiredArgsConstructor
+@Slf4j
 public class DaleController {
+    private static Logger logger = LoggerFactory.getLogger(DaleController.class);
     Long i = 0L;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<String> custom() {
+    public ResponseEntity<String> custom() throws InterruptedException {
         var statusList = List.of(HttpStatus.values());
-
+        logger.info("dale");
         Random random = new Random();
         int index = random.nextInt(statusList.size());
         HttpStatus randomStatus = statusList.get(index);
